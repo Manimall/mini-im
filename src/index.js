152c2f945aeda7b920e1e3950445f4d89195eb8f
Manimall <gorpunov94@mail.ru>
2019-11-06 18:43:49 +0300

    added hooks

---
+++ src/index.js
@@ -0,0 +1,37 @@
+import './main.css'
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import createSagaMiddleware from 'redux-saga';
+import {applyMiddleware, createStore} from 'redux'
+import {composeWithDevTools} from 'redux-devtools-extension'
+import {Provider} from 'react-redux'
+
+import {createBrowserHistory} from 'history'
+import {routerMiddleware, ConnectedRouter} from 'connected-react-router'
+
+// our own imports
+import App from 'App'
+import createRootReducer, {rootSaga} from 'modules'
+
+
+const sagaMiddleware = createSagaMiddleware();
+const history = createBrowserHistory();
+const middlewares = [sagaMiddleware, routerMiddleware(history)];
+
+const store = createStore(
+  createRootReducer(history),
+  composeWithDevTools(applyMiddleware(...middlewares))
+);
+
+sagaMiddleware.run(rootSaga);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <App />
+    </ConnectedRouter>
+  </Provider>,
+  document.getElementById('root')
+);
